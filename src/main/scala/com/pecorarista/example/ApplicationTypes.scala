ab0e372abb427ad6bb9f8b6154368d61979e15e8
Miyazawa Akira <pecorarista@gmail.com>
2019-11-28 18:17:45 +0900

    Update CircleCI settings

--- src/main/scala/com/pecorarista/example/ApplicationTypes.scala
+++ src/main/scala/com/pecorarista/example/ApplicationTypes.scala
@@ -2,15 +2,15 @@ package com.pecorarista.example
 
 object ApplicationTypes {
   case class Document(
-      uri: String,
-      title: String,
-      retrieved: Long,
-      content: String
+    uri: String,
+    title: String,
+    retrieved: Long,
+    content: String
   )
   case class UpdateDocumentRequest(
-      uri: String,
-      title: Option[String],
-      retrieved: Option[Long],
-      content: Option[String]
+    uri: String,
+    title: Option[String],
+    retrieved: Option[Long],
+    content: Option[String]
   )
 }
